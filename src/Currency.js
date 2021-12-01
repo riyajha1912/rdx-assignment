ae67b3674dcffe5e15a9617c829a0d38ffabbf88
RIYAJHA <riyajha1294@gmail.com>
2021-12-01 21:14:04 +0530

    Currency

--- src/Currency.js
+++ src/Currency.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import axios from "axios";
 const Currency = () => {
   const APIKEY = "AIzaSyDSjEjkbGBrbvEaa1RakqVKJ9uEWUDU2P0";
-  const placeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=newDelhi&key=${APIKEY}`;
+  // const placeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=newDelhi&key=${APIKEY}`;
   const country_info =
     "https://v2.api.forex/infos/currency/usd.json?beautify=true&highlat=2&key=42861af6-8d4e-47a2-b2ba-41df18a388c6";
 
