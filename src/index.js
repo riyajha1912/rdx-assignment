36688e92ee825b8e77bf9cc6e582c9768aff6495
RIYAJHA <riyajha1294@gmail.com>
2021-12-01 21:19:00 +0530

    weather and location

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Currency from "./Currency";
+import Maps from "./Maps";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Currency />
+    <Maps />
   </React.StrictMode>,
   document.getElementById("root")
 );
